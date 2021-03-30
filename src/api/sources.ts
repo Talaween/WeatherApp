04b7f672b41880b501ef1f9c0a3220f7e180ed49
Mahmoud Awad <awad@talaween.net>
2021-03-30 17:57:57 +0100

    hide api key in env and ignore in git

--- src/api/sources.ts
+++ src/api/sources.ts
@@ -1,5 +1,7 @@
 import {AppConfig} from '../App.config';
 
-export const currentWeatherAPI = (city:string, code:string) => `https://api.weatherbit.io/v2.0/current?city=${city},${code}&key=${AppConfig.apiKey}`;
+const API_KEY = process.env.REACT_APP_WEATHER_BIT_API_KEY;
 
-export const forecastWeatherAPI = (city:string, code:string) => `https://api.weatherbit.io/v2.0/forecast/daily?city=${city},${code}&key=${AppConfig.apiKey}`;
+export const currentWeatherAPI = (city:string, code:string) => `https://api.weatherbit.io/v2.0/current?city=${city},${code}&key=${API_KEY}`;
+
+export const forecastWeatherAPI = (city:string, code:string) => `https://api.weatherbit.io/v2.0/forecast/daily?city=${city},${code}&key=${API_KEY}`;
